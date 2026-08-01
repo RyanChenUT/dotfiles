ba4acf2b4d82d39e5266a74cccd31596d7107fcc
RyanChenUT <ryanrchen@utexas.edu>
2026-08-01 03:03:04 -0700

    kitty conf and bamboo colorscheme added

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,4 +9,28 @@ vim.cmd.colorscheme("gruvbox")
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>qq", ":qa<CR>", { desc = "Quit"})
+vim.keymap.set("n", "<leader><C-w>", ":wq<CR>", { desc = "Save Quit"})
 
+-- number lines
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.keymap.set({"n", "v"}, "<leader>l", function()
+  vim.opt.relativenumber = not vim.opt.relativenumber:get()
+end)
+
+-- lsp stuff
+vim.keymap.set("n", "<F12>", require("telescope.builtin").lsp_definitions, {
+  desc = "Go to definition",
+})
+
+vim.keymap.set("n", "<S-F12>", vim.lsp.buf.declaration, {
+  desc = "Go to declaration",
+})
+
+vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {
+  desc = "Find references",
+})
+
+vim.keymap.set("n", "<leader><C-e>", ":lua vim.diagnostic.open_float()<CR>", {
+  desc = "Elaborate Warning",
+})
